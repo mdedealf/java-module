ff39189d2b8381eca7c8282acb3d4417507ed7bb
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 20:37:59 +0700

    add:  adding scanner to all the inputs variables

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,41 @@
 import exercises.day2.*;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        double fahrenheit = 120;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input the temperature in Fahrenheit : ");
+        double fahrenheit = sc.nextDouble();
         CelciusConverter celciusConvert = new CelciusConverter(fahrenheit);
         double celcius = celciusConvert.convertFahrenheitToCelcius();
-        System.out.println(fahrenheit + " Fahrenheit to Celcius is : " + celcius + " °C");
-
-        double centimeter = 100000;
+        System.out.println(fahrenheit + " Fahrenheit to Celcius is : " + celcius + " °C\n");
+//
+        System.out.print("Input the length in centimeter : ");
+        double centimeter = sc.nextDouble();
         KilometersConverter centiToKilometers = new KilometersConverter(centimeter);
         double kilometers = centiToKilometers.convertCentimeterToKilometers();
-        System.out.println(centimeter + " cm to Kilometers is " + kilometers + " km");
-
-        int n = 4;
+        System.out.println(centimeter + " cm to Kilometers is " + kilometers + " km\n");
+//
+        System.out.print("Input the number to check : ");
+        int n = sc.nextInt();
+        sc.nextLine();
         OddChecker oddChecker = new OddChecker(n);
         boolean isOdd = oddChecker.isOdd();
-        System.out.println("is " + n + " is even number : " +isOdd);
+        System.out.println("is " + n + " even number : " + isOdd + "\n");
 
-        String words = "Hello World";
-        String stringToRemove = "ell";
+        System.out.print("Input the original words : ");
+        String words = sc.nextLine();
+
+        System.out.print("Input word or character to remove : ");
+        String stringToRemove = sc.nextLine();
+
         RemoveString removeString = new RemoveString(words, stringToRemove);
-        String result = removeString.removeTheFirstOccurrence();
-        System.out.println(result);
+        System.out.println(removeString.removeTheFirstOccurrence() + "\n");
 
-        String word = "kodok";
+        System.out.print("Input word to check if palindrome : ");
+        String word = sc.nextLine();
         PalindromeChecker palindromeChecker = new PalindromeChecker(word);
         boolean isPalindrome = palindromeChecker.isPalindrome();
 
@@ -31,5 +43,7 @@ public class Main {
             System.out.println(word + " is a palindrome");
         else
             System.out.println(word + " is not a palindrome");
+
+        sc.close();
     }
 }
