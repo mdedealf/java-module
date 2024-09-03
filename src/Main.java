05fb8a1ed00eb166203a2f38c4e456d69e77745a
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 15:53:20 +0700

    feat: initialize KilometersConverter class object to call that class method existed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import exercises.day2.CelciusConverter;
+import exercises.day2.KilometersConverter;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,5 +7,10 @@ public class Main {
         CelciusConverter celciusConvert = new CelciusConverter(fahrenheit);
         double celcius = celciusConvert.convertFahrenheitToCelcius();
         System.out.println(fahrenheit + " Fahrenheit to Celcius is : " + celcius + " °C");
+
+        double centimeter = 100000;
+        KilometersConverter centiToKilometers = new KilometersConverter(centimeter);
+        double kilometers = centiToKilometers.convertCentimeterToKilometers();
+        System.out.println(centimeter + " cm to Kilometers is " + kilometers + " km");
     }
 }
