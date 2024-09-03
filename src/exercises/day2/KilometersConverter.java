2b8f74f7d1f92d6ae148c4a5c2b08bb5e1be16ac
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 15:52:01 +0700

    feat: adding function to convert cm to km

--- src/exercises/day2/KilometersConverter.java
+++ src/exercises/day2/KilometersConverter.java
@@ -1,4 +1,13 @@
 package exercises.day2;
 
 public class KilometersConverter {
+    double centimeter;
+
+    public KilometersConverter(double centimeter) {
+        this.centimeter = centimeter;
+    }
+
+    public double convertCentimeterToKilometers() {
+        return centimeter / 1000;
+    }
 }
