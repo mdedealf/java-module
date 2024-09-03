6646c549fd3f5a440e64fa17c292381275804c01
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 16:19:53 +0700

    feat: add oddChecker function to check if input n is even or odd

--- src/exercises/day2/OddChecker.java
+++ src/exercises/day2/OddChecker.java
@@ -1,4 +1,13 @@
 package exercises.day2;
 
 public class OddChecker {
+    int n;
+
+    public OddChecker(int n) {
+        this.n = n;
+    }
+
+    public boolean isOdd() {
+        return n % 2 == 0;
+    }
 }
