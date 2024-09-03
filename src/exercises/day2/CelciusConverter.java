451f0aaccdd601d1c6366cf709aadbc02d07200c
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 15:47:11 +0700

    feat: adding fahrenheit to celcius calculation function

--- src/exercises/day2/CelciusConverter.java
+++ src/exercises/day2/CelciusConverter.java
@@ -1,5 +1,13 @@
 package exercises.day2;
 
 public class CelciusConverter {
+    double fahrenheit;
 
+    public CelciusConverter(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public double convertFahrenheitToCelcius() {
+        return (fahrenheit - 32) * 5 / 9;
+    }
 }
