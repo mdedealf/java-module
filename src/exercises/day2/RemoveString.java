ddb15a1e93d898e1f8e35f511da4a7618e579285
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 16:20:31 +0700

    feat: add function to remove the first string occurrence from the existance words

--- src/exercises/day2/RemoveString.java
+++ src/exercises/day2/RemoveString.java
@@ -1,4 +1,15 @@
 package exercises.day2;
 
 public class RemoveString {
+    String words;
+    String stringToRemove;
+
+    public RemoveString(String words, String stringToRemove) {
+        this.words = words;
+        this.stringToRemove = stringToRemove;
+    }
+
+    public String removeTheFirstOccurrence() {
+        return words.replaceFirst(stringToRemove, "");
+    }
 }
