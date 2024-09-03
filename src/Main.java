d934ed3f2f074326b081a4c0e69570700f2fd17b
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 16:21:33 +0700

    feat: initialize OddChecker object and String RemoveString object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import exercises.day2.CelciusConverter;
 import exercises.day2.KilometersConverter;
+import exercises.day2.OddChecker;
+import exercises.day2.RemoveString;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,5 +14,16 @@ public class Main {
         KilometersConverter centiToKilometers = new KilometersConverter(centimeter);
         double kilometers = centiToKilometers.convertCentimeterToKilometers();
         System.out.println(centimeter + " cm to Kilometers is " + kilometers + " km");
+
+        int n = 4;
+        OddChecker oddChecker = new OddChecker(n);
+        boolean isOdd = oddChecker.isOdd();
+        System.out.println("is " + n + " is even number : " +isOdd);
+
+        String words = "Hello World";
+        String stringToRemove = "ell";
+        RemoveString removeString = new RemoveString(words, stringToRemove);
+        String result = removeString.removeTheFirstOccurrence();
+        System.out.println(result);
     }
 }
