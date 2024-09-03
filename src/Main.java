d84ac8abaa8c19ee6907f703c437c42428ae57c3
mdedealf <dede.alfaruqq@gmail.com>
2024-09-03 15:47:46 +0700

    feat: adding object for CelciusConverter to call that function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import exercises.day2.CelciusConverter;
+
 public class Main {
     public static void main(String[] args) {
-
+        double fahrenheit = 120;
+        CelciusConverter celciusConvert = new CelciusConverter(fahrenheit);
+        double celcius = celciusConvert.convertFahrenheitToCelcius();
+        System.out.println(fahrenheit + " Fahrenheit to Celcius is : " + celcius + " °C");
     }
 }
